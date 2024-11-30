374763202708c98f26cc1d9f52cc09d636258196
alnaimi-github <alnymybdalwas1@gmail.com>
2024-11-30 21:17:10 +0300

    Add code formatting check and update WeatherForecast
    The `pr-verify.yml` file now includes a step to verify code formatting using `dotnet format`.
    The `Summary` property has been removed from the `WeatherForecast` record in `WeatherForecast.cs`.

--- src/GitHubActionsDotNet.Api/Models/WeatherForecast.cs
+++ src/GitHubActionsDotNet.Api/Models/WeatherForecast.cs
@@ -7,6 +7,5 @@ public record WeatherForecast
     public int TemperatureC { get; init; }
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
     public string? Summary { get; init; }
 }
